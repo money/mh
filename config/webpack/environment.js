d3fc52efc43e3f7a531a073743f8478e284805d1
money <manmohit.singh@gmail.com>
2020-06-08 08:20:24 -0700

    Add jQuery global variable to webpack

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
+ const webpack = require('webpack')
 
-module.exports = environment
+ environment.plugins.prepend('Provide',
+   new webpack.ProvidePlugin({
+     $: 'jquery',
+     jQuery: 'jquery'
+   })
+ )
+
+ module.exports = environment
