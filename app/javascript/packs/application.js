8b8adb5d56a2f1b3e7edd508f1fd2c3a76f8a23c
money <manmohit.singh@gmail.com>
2021-07-11 22:18:38 +1000

    Cleanup how js controllers are loaded

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,8 +4,7 @@ require("trix")
 import * as bootstrap from "bootstrap"
 import "@popperjs/core"
 import "@hotwired/turbo-rails"
-// import "controllers"
-require("controllers")
+import "controllers"
 
 document.addEventListener("trix-file-accept", function(event) {
   event.preventDefault()
